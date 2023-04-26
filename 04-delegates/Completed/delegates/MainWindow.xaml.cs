b7a1041df633d93a5800f5a336deba1d3bdb5f56
Jeremy Clark <jeremy@jeremybytes.com>
2023-04-26 04:13:15 -0700

    Added final step to delegates demo

--- 04-delegates/Completed/delegates/MainWindow.xaml.cs
+++ 04-delegates/Completed/delegates/MainWindow.xaml.cs
@@ -30,9 +30,9 @@ public partial class MainWindow : Window
         return null;
     }
 
-    private Action<List<Person>>? AssignAction()
+    private Action<List<Person>> GetAction()
     {
-        Action<List<Person>>? action = null;
+        Action<List<Person>> action = p => { };
 
         if (BestCommanderCheckBox.IsChecked!.Value)
             action += p => MessageBox.Show(
@@ -72,8 +72,8 @@ public partial class MainWindow : Window
         if (ActionExpander.IsExpanded)
         {
             // DO NOT DO THIS
-            // unless you hate your co-workers
-            AssignAction()?.Invoke(people);
+            // unless you really hate your co-workers
+            GetAction()(people);
         }
     }
 
